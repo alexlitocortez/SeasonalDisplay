51c9c947a0ea360fc0084b30a08a40ff9e498e8a
Alex Cortez <acim650@gmail.com>
2021-03-19 11:09:56 -0700

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './spinner';
+import "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
@@ -12,7 +14,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -21,7 +23,15 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>;
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
